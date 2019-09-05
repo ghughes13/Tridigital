724752a9cd1b3fd8d824639fca114efd2ba0de42
shuzaku <mtchau@gmail.com>
2019-09-05 16:03:50 -0500

    netlify stripped

--- src/components/footer.js
+++ src/components/footer.js
@@ -95,15 +95,6 @@ const Footer = () => (
                         <label>Your Email: <input type="email" name="email" /></label>
                     </p>
                     <p>
-                        <label>Your Role: <select name="role[]" multiple>
-                        <option value="leader">Leader</option>
-                        <option value="follower">Follower</option>
-                        </select></label>
-                    </p>
-                    <p>
-                        <label>Message: <textarea name="message"></textarea></label>
-                    </p>
-                    <p>
                         <button type="submit">Send</button>
                     </p>
                 </form>
