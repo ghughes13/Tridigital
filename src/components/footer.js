4c2fc132703007fa475875e75d0dd633b8a3e594
shuzaku <mtchau@gmail.com>
2019-09-05 15:41:01 -0500

    netlify forms

--- src/components/footer.js
+++ src/components/footer.js
@@ -87,12 +87,26 @@ const Footer = () => (
             <Col>
                 <h2 className="orange-text light-weight">Be the First!</h2>
                 <p className="white-text">Opt-in to our newsletter to stay updated with our latest news and happenings.</p>
-                <GravityFormForm
-                    id={2}
-                    formData={allGravityData()}
-                    presetValues={{ input_1: 'special_value' }}
-                    lambda={process.env.LAMBDA_ENDPOINT}
-                />
+                <form name="contact" method="POST" data-netlify="true">
+                    <p>
+                        <label>Your Name: <input type="text" name="name" /></label>   
+                    </p>
+                    <p>
+                        <label>Your Email: <input type="email" name="email" /></label>
+                    </p>
+                    <p>
+                        <label>Your Role: <select name="role[]" multiple>
+                        <option value="leader">Leader</option>
+                        <option value="follower">Follower</option>
+                        </select></label>
+                    </p>
+                    <p>
+                        <label>Message: <textarea name="message"></textarea></label>
+                    </p>
+                    <p>
+                        <button type="submit">Send</button>
+                    </p>
+                </form>
 
             </Col>
 
