d5eea0058e55ad76fa006b61cbda6b84d5ac4213
Garrett <guitarguy13@ymail.com>
2020-02-20 15:51:21 -0600

    added svgs to hero background

--- src/components/warmupSignupComps/checkoutForm.js
+++ src/components/warmupSignupComps/checkoutForm.js
@@ -2,39 +2,39 @@ import React from "react";
 
 const CheckoutForm = () => {
   return (
-    // <form
-    //   name="contact"
-    //   method="POST"
-    //   data-netlify="true"
-    //   netlify-honeypot="bot-field"
-    // >
-    //   <input type="text" name="name" placeholder="Name" />
-    //   <input type="email" name="email" placeholder="Email" />
-    //   <input type="hidden" name="form-name" value="contact" />
-    //   <button type="submit">Subscribe</button>
-    // </form>
-
     <form
-      name="warmup"
-      data-netlify="true"
+      name="contact"
       method="POST"
+      data-netlify="true"
       netlify-honeypot="bot-field"
     >
-      <h3 className="blue-text form-title">
-        Complete The Sign Up Form to Get Your Marketing Started
-      </h3>
-      <div className="name-container">
-        <div className="firstName">
-          <label>First Name</label>
-          <input type="text" name="firstName" />
-        </div>
-        <div className="lastName">
-          <label>Last Name</label>
-          <input type="text" name="LastName" />
-        </div>
-      </div>
-      <button type="submit">Send</button>
+      <input type="text" name="name" placeholder="Name" />
+      <input type="email" name="email" placeholder="Email" />
+      <input type="hidden" name="form-name" value="contact" />
+      <button type="submit">Subscribe</button>
     </form>
+
+    // <form
+    //   name="warmup"
+    //   data-netlify="true"
+    //   method="POST"
+    //   netlify-honeypot="bot-field"
+    // >
+    //   <h3 className="blue-text form-title">
+    //     Complete The Sign Up Form to Get Your Marketing Started
+    //   </h3>
+    //   <div className="name-container">
+    //     <div className="firstName">
+    //       <label>First Name</label>
+    //       <input type="text" name="first" />
+    //     </div>
+    //     <div className="lastName">
+    //       <label>Last Name</label>
+    //       <input type="text" name="Last" />
+    //     </div>
+    //   </div>
+    //   <button type="submit">Subscribe</button>
+    // </form>
   );
 };
 
