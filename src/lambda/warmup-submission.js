db2c25f1ecd7693270486a70fa4d4a69877d6185
Bill Wettingfeld <bwettingfeld@TRIDIGITAL.COM>
2020-02-20 16:30:51 -0600

    Updated naming convention of lambda

--- src/lambda/warmup-submission.js
+++ src/lambda/warmup-submission.js
@@ -2,11 +2,11 @@ exports.handler = function(event, context, callback) {
     // add form submission contents
     // add stripe call
 
+    console.log(event);
+    console.log(context);
+
     callback(null, {
         statusCode: 200,
-        body: {
-            event: event,
-            context: context
-        }
+        body: "Hello, World"
     });
 }
