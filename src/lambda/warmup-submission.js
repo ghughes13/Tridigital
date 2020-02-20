961b61890a4ef46cb1652dbb604baf5d19c76535
Bill Wettingfeld <bwettingfeld@TRIDIGITAL.COM>
2020-02-20 16:49:43 -0600

    Added valid body check

--- src/lambda/warmup-submission.js
+++ src/lambda/warmup-submission.js
@@ -1,12 +1,54 @@
+//require("dotenv").config();
+
+//const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "Content-Type"
+};
+
+function hasValidBody(body) {
+    return false;
+}
+
 exports.handler = function(event, context, callback) {
+    var body = JSON.parse(context.body);
+    
+    if (!hasValidBody(body)) {
+        const message = "Please fill out all required information.";
+        console.error(message);
+    
+        callback(null, {
+          statusCode: 400,
+          headers,
+          body: JSON.stringify({
+            status: "Failed",
+            message
+          })
+        });
+    }
+
     // add form submission contents
+
     // add stripe call
+    // {
+    //     firstName,
+    //     lastName,
+    //     companyName,
+    //     email,
+    //     ccNumber,
+    //     ccExpirationMonth,
+    //     ccExpirationYear,
+    //     cvv,
+    //     priceTierId
+    // }
+
+
 
     console.log(event);
     console.log(context);
 
     callback(null, {
-        statusCode: 200,
-        body: "Hello, World"
+        statusCode: 200
     });
 }
