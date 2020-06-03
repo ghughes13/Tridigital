6c3ae5bbc734242e1711adeebeb91211e321a6f1
ghughes13 <guitarguy13@ymail.com>
2020-06-03 08:27:40 -0500

    added ibm video

--- src/pages/ingram-ibm-program.js
+++ src/pages/ingram-ibm-program.js
@@ -26,14 +26,14 @@ const VendorPartnerLP = () => {
             <Col className="content-container column align-vertical video-container ">
               <video
                 className="lp-video box-shadow"
-                src="https://encrypted.tridigitalmarketing.com/wp-content/uploads/vendor-lp-video/Vendor_15_sec_loop_AA_V01%20%281%29.mp4"
+                src="https://encrypted.tridigitalmarketing.com/wp-content/uploads/vendor-lp-video/Ingram%20Micro%20%26amp%3B%20IBM%20Partner%20Enablement.mp4"
                 alt="TRIdigital media team"
                 id="video-player"
                 autoPlay
                 loop
                 muted
               ></video>
-              <Yourvideo videoID={"3iwu5wakg8"} />
+              <Yourvideo videoID={"8lk2vobcok"} />
             </Col>
           </Row>
         </Container>
