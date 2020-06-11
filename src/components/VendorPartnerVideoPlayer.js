4fa0c9c6fc79c681b8a34ebe49e102ed4557189a
ghughes13 <guitarguy13@ymail.com>
2020-06-11 13:27:26 -0500

    removed console.logs

--- src/components/VendorPartnerVideoPlayer.js
+++ src/components/VendorPartnerVideoPlayer.js
@@ -5,6 +5,11 @@ const YourVideo = props => {
   const [videoID] = useState(props.videoID);
 
   useEffect(() => {
+    if (window.innerWidth < 767) {
+      console.log("setit");
+      setIsMobile(true);
+    }
+
     const script1 = document.createElement("script");
     const script2 = document.createElement("script");
 
@@ -16,52 +21,47 @@ const YourVideo = props => {
 
     document.body.appendChild(script1);
     document.body.appendChild(script2);
-
-    if (window.innerWidth < 767) {
-      console.log("setit");
-      setIsMobile(true);
-    }
-
-    console.log(isMobile);
   }, []);
 
-  console.log(isMobile);
-
-  return isMobile ? (
-    <div className="video-btn mobile-video-btn">
-      <span
-        className={
-          "wistia_embed wistia_async_" +
-          videoID +
-          " popover=true popoverAnimateThumbnail=true popoverContent=link"
-        }
-      >
-        <button href="#">
-          <img
-            src="https://encrypted.tridigitalmarketing.com/wp-content/uploads/2020/06/Capture.png"
-            alt="play button"
-          ></img>
-        </button>
-      </span>
-    </div>
-  ) : (
-    <div className="video-btn">
-      <span
-        className={
-          "wistia_embed wistia_async_" +
-          videoID +
-          " popover=true popoverAnimateThumbnail=true popoverContent=link"
-        }
-      >
-        <button href="#">
-          <img
-            src="https://encrypted.tridigitalmarketing.com/wp-content/uploads/2020/01/Group-3451-2.png"
-            alt="play button"
-          ></img>
-        </button>
-      </span>
-    </div>
-  );
+  if (isMobile) {
+    return (
+      <div className="video-btn mobile-video-btn">
+        <span
+          className={
+            "wistia_embed wistia_async_" +
+            videoID +
+            " popover=true popoverAnimateThumbnail=true popoverContent=link"
+          }
+        >
+          <button href="#">
+            <img
+              src="https://encrypted.tridigitalmarketing.com/wp-content/uploads/2020/06/Capture.png"
+              alt="play button"
+            ></img>
+          </button>
+        </span>
+      </div>
+    );
+  } else {
+    return (
+      <div className="video-btn">
+        <span
+          className={
+            "wistia_embed wistia_async_" +
+            videoID +
+            " popover=true popoverAnimateThumbnail=true popoverContent=link"
+          }
+        >
+          <button href="#">
+            <img
+              src="https://encrypted.tridigitalmarketing.com/wp-content/uploads/2020/01/Group-3451-2.png"
+              alt="play button"
+            ></img>
+          </button>
+        </span>
+      </div>
+    );
+  }
 };
 
 export default YourVideo;
